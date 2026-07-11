d448c3931804361536b11e357f50913f2b0ba100
Derek Petersen <tuxracer@gmail.com>
2026-07-11 00:14:28 -0700

    Initial release

--- components/Loading/index.tsx
+++
@@ -1,9 +0,0 @@
-export const Loading = () => {
-    return (
-        <div className="loading-layout">
-            <div className="loading-outline">
-                <div className="loading-progress"></div>
-            </div>
-        </div>
-    );
-};
